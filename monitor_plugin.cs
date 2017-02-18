517e108583a757dde164094fb9b348703170ddd3
unknown <peter.sovietov@gmail.com>
2017-02-18 12:56:56 +0300

    beter get_sensor_by_path

--- monitor_plugin.cs
+++ monitor_plugin.cs
@@ -69,36 +69,18 @@ class Program {
 
   static float get_sensor_by_path(string section, string sensor_type, string sensor_name) {
     List<dynamic> section_list = sensors[section].ToObject<List<dynamic>>();
-    if (section_list.Count == 0) {
-      return 0;
-    }
-    List<dynamic> sensor_list = section_list[0][sensor_type].ToObject<List<dynamic>>();
-    if (sensor_list.Count == 0) {
-      return 0;
-    }
-    foreach (var x in sensor_list) {
-      if (x["name"] == sensor_name) {
-        return x["value"];
+    foreach (var device in section_list) {
+      List<dynamic> sensor_list = device[sensor_type].ToObject<List<dynamic>>();
+      if (sensor_list.Count != 0) {
+        foreach (var sensor in sensor_list) {
+          if (sensor["name"] == sensor_name) {
+            return sensor["value"];
+          }
+        }
+        return sensor_list[0]["value"];
       }
     }
-    return sensor_list[0]["value"];
-  }
-
-  static float get_gpu_sensor_by_path(string section, string sensor_type, string sensor_name) {
-    List<dynamic> section_list = sensors[section].ToObject<List<dynamic>>();
-    if (section_list.Count != 2) {
-      return 0;
-    }
-    List<dynamic> sensor_list = section_list[1][sensor_type].ToObject<List<dynamic>>();
-    if (sensor_list.Count == 0) {
-      return 0;
-    }
-    foreach (var x in sensor_list) {
-      if (x["name"] == sensor_name) {
-        return x["value"];
-      }
-    }
-    return sensor_list[0]["value"];
+    return 0;
   }
 
   static float get_sensor(Effect e) {
@@ -110,10 +92,10 @@ class Program {
       x = get_sensor_by_path("cpus", "loads", "UC");
       x = Tools.remap(x, 0, 100, 0, 1);
     } else if (e.name == "gpu_temp") {
-      x = get_gpu_sensor_by_path("gpus", "temps", "GPU");
+      x = get_sensor_by_path("gpus", "temps", "GPU");
       x = Tools.remap(x, 40, 70, 0, 1);
     } else if (e.name == "gpu_load") {
-      x = get_gpu_sensor_by_path("gpus", "loads", "GPU");
+      x = get_sensor_by_path("gpus", "loads", "GPU");
       x = Tools.remap(x, 0, 100, 0, 1);
     }
     return x;
